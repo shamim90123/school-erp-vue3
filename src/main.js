5253c6ee1a271da0963a3b959eba59cd86b388c0
Md Shamim Reza <opt.shamim@gmail.com>
2023-07-19 21:58:15 +0600

    i18n sample implementation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router/router'
+
+// // createApp(App).mount('#app')
+// createApp(App).use(router).mount('#app');
+
 import { createApp } from 'vue'
 import App from './App.vue'
+import i18n from './i18n'
 import router from './router/router'
 
-// createApp(App).mount('#app')
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
 
+app.use(router);
+
+// Use the i18n plugin
+app.use(i18n);
+
+app.mount('#app');
