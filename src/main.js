d350b20b567f076b26cf4e13d9e535bf4a3f10fa
Md Shamim Reza <opt.shamim@gmail.com>
2023-07-20 21:24:51 +0600

    code review

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router/router'
-
-// // createApp(App).mount('#app')
-// createApp(App).use(router).mount('#app');
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
