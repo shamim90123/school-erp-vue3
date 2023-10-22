e71eb25e96e0679b3adcea5ab57e816a681cef94
Md Shamim Reza <opt.shamim@gmail.com>
2023-10-22 20:08:07 +0600

    scss implementation

--- src/components/modules/auth/Login.vue
+++ src/components/modules/auth/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login-container">
-    <div class="login-card">
+    <div class="login-card my-component">
+      <p>SCSS implementation!</p>
       <h2>Login <button @click="changeLanguage" class="btn btn-sm btn-success" style="width:auto;float: right;">{{ $i18n.locale == 'en' ? 'Bn' : 'En' }}</button></h2>
   
       <form @submit.prevent="login">
@@ -58,47 +59,14 @@
   };
 </script>
 
-<style>
-.login-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f2f2f2;
-}
 
-.login-card {
-  max-width: 400px;
-  padding: 40px;
-  border-radius: 4px;
-  background-color: #fff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
-}
-
-h2 {
-  text-align: center;
-  color: #333;
-}
-
-input {
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-
-button {
-  width: 100%;
-  padding: 10px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-button:hover {
-  background-color: #0056b3;
+<style lang="scss">
+// Define SaaS styles here
+$primary-color: #007bff;
+.my-component {
+  color: $primary-color;
+  p {
+    font-size: 20px;
+  }
 }
 </style>
--- src/config/api_config.js
+++ src/config/api_config.js
@@ -1,1 +1,2 @@
-export const authServiceBaseURL = 'http://127.0.0.1:8000'
+export const authServiceBaseURL = 'http://school-auth-service.local/'
+export const commonServiceBaseURl = 'http://school-common-service.local/'
